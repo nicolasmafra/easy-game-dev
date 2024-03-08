24e7daf9dfcfb90ea5f832cb586cc6613275cc99
Nicolas Mafra <nickmafra@hotmail.com>
2024-03-08 20:07:35 -0300

    feat: proportional size

--- public/puzzle/gfx.js
+++ public/puzzle/gfx.js
@@ -1,16 +1,26 @@
-const gridSize = 40;
-const cellSize = gridSize - 2;
-const offset = gridSize;
-
 let canvas = document.createElement('canvas');
-canvas.width = window.innerWidth - 20;
-canvas.height = window.innerHeight - 20;
 document.body.appendChild(canvas);
 
 let ctx = canvas.getContext('2d');
-ctx.font = '20px sans serif';
 ctx.textAlign = 'center';
 ctx.textBaseline = 'middle';
+
+let gridSize;
+let cellSize;
+let offset;
+let fontSize;
+
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    gridSize = Math.min(canvas.width/(cols+1), canvas.height/(rows+1));
+    cellSize = gridSize - 2;
+    offset = gridSize;
+    fontSize = gridSize/2;
+    
+    ctx.font = `${fontSize}px sans serif`;
+}
 
 function calcColor(position) {
     let r = 100 + Math.round(100*position.row/(rows-1));
@@ -27,8 +37,14 @@ function draw(state) {
             let y = offset + cell.row * gridSize;
             ctx.fillStyle = calcColor(originalPosition(cell.v));
             ctx.fillRect(x - cellSize/2, y - cellSize/2, cellSize, cellSize);
-            ctx.fillStyle = 'black';
-            ctx.fillText(cell.v, x, y);
+        }
+    });
+    ctx.fillStyle = 'black';
+    state.forEach(cell => {
+        if (cell.v) {
+            let x = offset + cell.col * gridSize;
+            let y = offset + cell.row * gridSize;
+            ctx.fillText(cell.v, x - fontSize/2, y + fontSize/2);
         }
     });
 }
